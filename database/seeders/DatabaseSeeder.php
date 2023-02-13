42ec176fa10d918dd8a4faa220eaf0bbbc99390b
Affan Yusuf <affanproject18@gmail.com>
2023-02-14 00:07:11 +0700

    feature-add-db-seeder :sparkles:

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        \App\Models\Category::factory(10)->create();
+        \App\Models\Image::factory(10)->create();
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
